4561b97424440fa8f9ed77a02d2d7042bbeb32a4
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-09-13 11:21:25 +0300

    csp improvement

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    middleware({
+      setup: (app) => {
+        app.use((req, res, next) => {
+          res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-<random-value>'; style-src 'self' 'nonce-<random-value>'; img-src 'self' data:; connect-src 'self'; font-src 'self'; frame-src 'none';");
+          next();
+        });
+      },
+    }),
+  ],
   server:{
     host: '0.0.0.0',
     port: 3000,
