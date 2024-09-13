30b4d2260172ca26eb04aaccbb21dee619fe0019
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-09-13 11:22:33 +0300

    csp

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import middleware from 'vite-plugin-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
