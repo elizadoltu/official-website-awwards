c4691febc0c767565f2ffa8b87c8aaa996da66f2
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-09-13 11:24:41 +0300

    more csp

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import middleware from 'vite-plugin-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +8,5 @@ export default defineConfig({
   server:{
     host: '0.0.0.0',
     port: 3000,
-    middlewareMode: true,
-    setup: (app) => {
-      app.use((req, res, next) => {
-        res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-<random-value>'; style-src 'self' 'nonce-<random-value>'; img-src 'self' data:; connect-src 'self'; font-src 'self'; frame-src 'none';");
-        next();
-      });
-    },
   },
 })
